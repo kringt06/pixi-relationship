8344bb668e6979065428771e1a8d31291ac31d66
tangqitao <364734461@qq.com>
2019-07-06 13:54:19 +0800

    tqt update master

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,51 @@
-import babel from "rollup-plugin-babel";
-import buble from "rollup-plugin-buble";
-import resolve from "rollup-plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
-import commonjs from "rollup-plugin-commonjs";
-import builtins from "rollup-plugin-node-builtins";
-// import image from "rollup-plugin-img";
+import babel from "rollup-plugin-babel"
+import buble from "rollup-plugin-buble"
+import resolve from "rollup-plugin-node-resolve"
+import { terser } from "rollup-plugin-terser"
+import commonjs from "rollup-plugin-commonjs"
+import builtins from "rollup-plugin-node-builtins"
+import url from "rollup-plugin-url"
+import alias from "rollup-plugin-alias"
+import path from "path"
 
-import pkg from "./package.json";
+import pkg from "./package.json"
 
 const plugins = [
   builtins(),
-  // image({
-  //   limit: 1024 * 100
-  // }),
+  url({
+    limit: 20 * 1024, // inline files < 20k, copy files > 20k
+    include: [/.(png|jpg|svg|gif)$/],
+    fileName: "[dirname][hash][extname]",
+    publicPath: `${pkg.name}/public/`
+  }),
+  alias({
+    resolve: ["", ".js", "/index.js"],
+    "@": path.resolve(__dirname, "src"),
+    [`${pkg.name}/public`]: path.resolve(__dirname, "public")
+  }),
+  babel({
+    babelrc: false,
+    exclude: ["node_modules/**"],
+    plugins: [
+      [
+        "@babel/plugin-proposal-class-properties",
+        {
+          loose: true
+        }
+      ]
+    ]
+  }),
   resolve(),
   commonjs({
     namedExports: {
       "resource-loader": ["Resource"]
     }
   }),
-  babel({
-    babelrc: false,
-    exclude: ["node_modules/**"]
-  }),
   buble()
-];
+]
 
 if (process.env.NODE_ENV === "production") {
-  plugins.push(terser());
+  plugins.push(terser())
 }
 
 const globals = {
@@ -41,9 +59,9 @@ const globals = {
   "@pixi/constants": "PIXI",
   "@pixi/display": "PIXI",
   "@pixi/runner": "PIXI"
-};
+}
 
-const external = Object.keys(globals);
+const external = Object.keys(globals)
 
 export default [
   {
@@ -55,4 +73,4 @@ export default [
     ],
     plugins
   }
-];
+]
